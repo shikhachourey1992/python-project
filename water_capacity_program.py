ebff600fbe83b7d92bd11214f15b14a300e2dd06
shikhachourey1992 <shikhachourey1992@gmail.com>
2026-04-09 16:25:04 +0530

    Add files via upload

---
+++ water_capacity_program.py
@@ -0,0 +1,71 @@
+# Smart Water Bottle  Create a class WaterBottle with:
+# capacity (in ml)
+# current_level
+# Methods:
+# drink(amount) → reduces water level
+# refill() → fills bottle to full capacity
+# If user tries to drink more than available, print “Not enough water”
+# Password Strength Checker
+#
+# Create a class Password:
+#
+# Attribute: password
+# Method strength() that prints:
+# Weak (length < 6)
+# Medium (length ≥ 6)
+# Strong (length ≥ 8 and contains a digit)
+#  Online Food Order
+#
+# Create a class FoodOrder:
+#
+# item_name
+# price
+# quantity
+# Methods:
+#
+# total_bill() → returns total price
+# apply_discount() → if bill > 500, give 10% discount
+
+#Smart Water Bottle  Create a class WaterBottle with:
+# capacity (in ml)
+# current_level
+# Methods:
+# drink(amount) → reduces water level
+# refill() → fills bottle to full capacity
+# If user tries to drink more than available, print “Not enough water”
+
+# class WaterBottle:
+#     def __init__(self,capacity,current_level):
+#         self.capacity = capacity
+#         self.current_level = current_level
+#     def drink(self,amount):
+#         self.current_level -= amount
+#         print("reduce water level")
+#     def refill(self):
+#         self.current_level = self.capacity
+#         print("refill water level")
+# wb=WaterBottle(capacity=20,current_level=5)
+# wb.drink(10)
+# wb.refill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
